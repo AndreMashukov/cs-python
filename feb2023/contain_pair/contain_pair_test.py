4cb94a7d50a512528b30058de93f3e94d9a3c7f2
Andrey Mashukov <andre.mashukov@gmail.com>
2023-03-25 16:28:03 +0800

    得閒先至講俾你聽啦

---
+++ feb2023/contain_pair/contain_pair_test.py
@@ -0,0 +1,22 @@
+import unittest
+from contain_pair import contain_pair
+
+class ContainPairTest(unittest.TestCase):
+  def test_rotated_rect_sum_1(self):
+    a = [2, 4, 7, 5, 3, 5, 8, 5, 1, 7]
+    m = 4
+    k = 10
+
+    result = contain_pair(a, m, k)
+    self.assertEqual(result, 5)
+
+  def test_rotated_rect_sum_2(self):
+    a = [15, 8, 8, 2, 6, 4, 1, 7]
+    m = 2
+    k = 8
+
+    result = contain_pair(a, m, k)
+    self.assertEqual(result, 2)
+
+if __name__ == "__main__":
+  unittest.main()
