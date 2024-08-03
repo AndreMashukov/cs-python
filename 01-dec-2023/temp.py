bf36e7171ae4d1925f341f0a1048a65fd88f5abf
Andrey Mashukov <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>
2024-08-03 15:26:39 +0800

    changes

--- 01-dec-2023/temp.py
+++ 01-dec-2023/temp.py
@@ -1,14 +1,27 @@
-# Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two neighboring numbers is equal (note that 0 and n - 1 are neighboring, too).
+import re
+from collections import defaultdict
 
-# Given n and firstNumber, find the number which is written in the radially opposite position to firstNumber.
+def rare_words_finder(text):
+    # implement this
+    words = text.lower().split()
+    word_counts = defaultdict(int)
+    
+    for word in words:
+        word_counts[word] += 1
+        
+    rarest_words = []
+    max_count = float('inf')
+    for word, count in word_counts.items():
+        if count < max_count:
+            rarest_words = [(word, count)]
+            max_count = count
+        elif count == max_count:
+            rarest_words.append((word, count))
+    #limit the output to only five words.
+    return rarest_words[:5]
 
-# Example
+print(rare_words_finder("Hey there hot shot Are you ready for a challenge This might be trickier than it looks")) # Expected Output: [('hey', 1), ('there', 1), ('hot', 1), ('shot', 1), ('are', 1)]
 
-# For n = 10 and firstNumber = 2, the output should be
-# solution(n, firstNumber) = 7.
+print(rare_words_finder("The quick brown fox jumps over the lazy dog The fox is quick but the dog is lazy")) # Expected Output: [('brown', 1), ('jumps', 1), ('over', 1), ('but', 1), ('is', 2)]
 
-def solution(n, firstNumber):
-  if firstNumber < n/2:
-    return firstNumber + n/2
-  else:
-    return firstNumber - n/2
+print(rare_words_finder("")) # Expected Output: []
